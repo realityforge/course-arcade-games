c203d372db66bacf2a50bbbdc4f076ac1a5656b4
Peter Donald <peter@realityforge.org>
2019-08-29 21:09:18 +1000

    Fix condition

--- src/main/java/org/realityforge/arcade/breakout/Breakout.java
+++ src/main/java/org/realityforge/arcade/breakout/Breakout.java
@@ -210,7 +210,7 @@ public class Breakout
     {
       // Ensure that if the ball is coming down because it has somehow got above
       // the world then let it continue coming down
-      if ( _ballSpeedX < 0 )
+      if ( _ballSpeedY < 0 )
       {
         _ballSpeedY = -_ballSpeedY;
       }
